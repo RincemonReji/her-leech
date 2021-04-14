73ac6c043ff383947a0370d216bf788ae8050759
Amirul Andalib <70439168+AmirulAndalib@users.noreply.github.com>
2021-04-14 20:44:04 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-pip3 install youtube-dl; python3 -m tobrot
+pip3 install youtube-dl; 
+if [[ -n $RCLONE_CONFIG ]]; then
+ echo "Rclone config detected"
+ echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+fi
+python3 -m tobrot
